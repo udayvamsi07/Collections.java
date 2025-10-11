222698d2409ff5e1d5d5f4c972543d67b84a43aa
uday vamsi pitta <udayvamsi07@gmail.com>
2025-10-11 12:44:11 -0400

    hashmapp

---
+++ src/pack/Hashmapp.java
@@ -0,0 +1,20 @@
+package pack;
+import java.util.Map;
+import java.util.HashMap;
+//Hashmap and hashtable are similar but the only difference is hashmap will not sycronize the data and hashtable will syncronize the data if multiple there are multilple threads are there.
+public class Hashmapp {
+    public static void main(String[]args)
+    {
+        Map<String,Integer> marks=new HashMap<>();
+        marks.put("maths",89);
+        marks.put("science", 90);
+        marks.put("commerce",88);
+        System.out.println(marks);
+        System.out.println(marks.get("maths"));
+        for(String key:marks.keySet())
+        {
+            System.out.println(key+":"+marks.get(key));
+        }
+        
+    }
+}
