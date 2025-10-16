88fe367ce5b65015b6477857fca7a331e90a4938
uday vamsi pitta <udayvamsi07@gmail.com>
2025-10-16 14:23:20 -0400

    optionall class

---
+++ src/Optionall.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Optionall {
+    public static void main(String[] args) {
+        List<String> names=Arrays.asList("uday","vamsi","Pitta","batman");
+       Optional<String> str=names.stream()
+              .filter(st ->st.contains("n"))
+              .findFirst();
+
+        System.out.println(str.orElse("not found"));
+        
+
+    }
+
+}
