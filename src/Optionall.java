40fe8188a263c863df1395aae89dd448189c6fe7
uday vamsi pitta <udayvamsi07@gmail.com>
2025-10-16 14:28:09 -0400

    optionall

--- src/Optionall.java
+++ src/Optionall.java
@@ -7,13 +7,19 @@ import java.util.Optional;
 public class Optionall {
     public static void main(String[] args) {
         List<String> names=Arrays.asList("uday","vamsi","Pitta","batman");
-       Optional<String> str=names.stream()
+    //    Optional<String> str=names.stream()
+    //           .filter(st ->st.contains("n"))
+    //           .findFirst();
+
+    //     System.out.println(str.orElse("not found"));
+
+//or we can use this way
+String str=names.stream()
               .filter(st ->st.contains("n"))
-              .findFirst();
+              .findFirst()
+              .orElse("not found");
 
-        System.out.println(str.orElse("not found"));
-        
-
+        System.out.println(str);
     }
 
 }
